5c3b413a34612bc4c949d455a018ec7a54ff930c
daiki-nishimura <daikiahe0@gmail.com>
2021-04-25 12:20:47 +0900

    おみくじbot

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,15 @@
 'use strict';
+
 module.exports = robot => {
   robot.hear(/hello>/i, msg => {
-    const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
+    const username= msg.message.user.id;
+    msg.send(`Hello, <@${username}>`);
+  });
+  robot.hear(/おみくじ/i, msg => {
+    const username = msg.message.user.id;
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`Hello, <@${username}>`);
   });
 };
+
